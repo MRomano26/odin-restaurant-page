ecaf06956e5056d1820faf52261fbb42c066baf1
MRomano26 <michael.mar26@gmail.com>
2021-12-13 15:20:13 -0500

    Adjusting webpack for development

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: 'index.js',
+    entry: './src/index.js',
     devtool: 'inline-source-map',
     output: {
-        __filename: 'bundle.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
+        //clean: true,
     },
 };
