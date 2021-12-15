cc3b942a5c0238e85a2f764d4f95874830e89f87
MRomano26 <michael.mar26@gmail.com>
2021-12-15 17:16:45 -0500

    Create javascript module function that will control the html document

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './style.css';
+import SteakImage from './assets/steak.avif';
 
+const displayController = (function(){
+    const _body = document.querySelector('body');
+    const _createNavbar = function() {
+
+    }
+
+    const _createHome = function() {
+
+    }
+
+    const startPage = function() {
+        _createNavbar();
+        _createHome();
+    }
+    return {startPage};
+})();
+
+displayController.startPage();
