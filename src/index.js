8ae1f024909dadf696ccb5b4a39fb14fdbeeb012
MRomano26 <michael.mar26@gmail.com>
2021-12-15 17:42:51 -0500

    Create navbar with javascript

--- src/index.js
+++ src/index.js
@@ -4,7 +4,37 @@ import SteakImage from './assets/steak.avif';
 const displayController = (function(){
     const _body = document.querySelector('body');
     const _createNavbar = function() {
+        const navbar = document.createElement('div');
+        navbar.id = 'navbar';
 
+        // Logo and title
+
+        const logoAndTitle = document.createElement('div');
+        const logo = document.createElement('div');
+        const title = document.createElement('div');
+        logo.id = 'logo';
+        logo.innerHTML = '&#129385;';
+        title.id = 'title';
+        title.textContent = 'Steak Steakhouse';
+        logoAndTitle.appendChild(logo);
+        logoAndTitle.appendChild(title);
+        navbar.appendChild(logoAndTitle);
+
+        // Other Pages
+
+        const pageList = document.createElement('ul');
+        const home = document.createElement('li');
+        const menu = document.createElement('li');
+        const contact = document.createElement('li');
+        home.textContent = 'Home';
+        menu.textContent = 'Menu';
+        contact.textContent = 'Contact';
+        pageList.appendChild(home);
+        pageList.appendChild(menu);
+        pageList.appendChild(contact);
+        navbar.appendChild(pageList);
+
+        _body.appendChild(navbar);
     }
 
     const _createHome = function() {
